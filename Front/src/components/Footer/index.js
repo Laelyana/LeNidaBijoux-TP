5665800c24dc7232066c10d032f5dbfe0d1b39ed
PiMa31 <pierre.masset@bbox.fr>
2021-02-11 20:56:45 +0100

    footer: add link and social network logo

--- Front/src/components/Footer/index.js
+++ Front/src/components/Footer/index.js
@@ -4,25 +4,28 @@ import PropTypes from 'prop-types';
 
 import './footer.scss';
 
+import faecebookLogo from './facebook.png';
+import instagramLogo from './instagram.png';
+
 const Footer = () => (
   <footer className="footer">
     <div className="footer__legals">
       <ul className="footer__legals--ul">
-        <li className="footer__legals--li">Mentions légales</li>
-        <li className="footer__legals--li">Conditions générales de vente</li>
+        <a href="#"><li className="footer__legals--li">Mentions légales</li></a>
+        <a href="#"><li className="footer__legals--li">Conditions générales de vente</li></a>
       </ul>
     </div>
     <div className="footer__social">
       <div className="footer__social--img">
-        <img  src="#" alt="logo FB" />
-        <img  src="#" alt="logo Insta" />
+        <a href="#"><img className="socialLogo" src={faecebookLogo} alt="logo FB" /></a>
+        <a href="#"><img className="socialLogo" src={instagramLogo} alt="logo Insta" /></a>
       </div>
     </div>
     <div className="footer__contact">
       <ul className="footer__contact--ul">
-        <li className="footer_contact--li">Adresse</li>
-        <li className="footer_contact--li">Tel</li>
-        <li className="footer_contact--li">Lien vers formulaire de contact</li>
+        <li className="footer_contact--li">1 rue par ici 31000 Parla</li>
+        <li className="footer_contact--li">0123456789</li>
+        <a href="#"><li className="footer_contact--li">Contactez nous</li></a>
       </ul>
     </div>
   </footer>
