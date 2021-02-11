c56a19c8f0cbdf3f7f61745f53fdfecec4dccf71
BenoitLetessier <ciib@free.fr>
2021-02-11 09:56:51 +0100

    Front BL Slider mise en place

--- Front/src/components/App/index.js
+++ Front/src/components/App/index.js
@@ -2,14 +2,13 @@
 import React from 'react';
 
 // == Import
-import reactLogo from './img.jpg';
+import Slider from 'src/components/Slider';
 import './styles.scss';
 
 // == Composant
 const App = () => (
   <div className="app">
-    <img src={reactLogo} alt="react logo" />
-    <h1>Projet VP Bijoux Front Power</h1>
+  <Slider />
   </div>
 );
 
---
+++ Front/src/components/Slider/index.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+
+import './slider.scss';
+
+const Form = () => (
+  <div className="slider">test</div>
+);
+
+
+
+export default Slider;
