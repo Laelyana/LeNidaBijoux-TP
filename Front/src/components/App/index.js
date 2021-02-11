8d93f2be51e5169a2ec2ab013af007dc56223843
PiMa31 <pierre.masset@bbox.fr>
2021-02-11 13:38:12 +0100

    merge ok

--- Front/src/components/App/index.js
+++ Front/src/components/App/index.js
@@ -7,13 +7,14 @@ import './styles.scss';
 // Import Local
 import Bandeau from 'src/components/Bandeau';
 import Header from 'src/components/Header';
+import Slider from 'src/components/Slider';
 
 // == Composant
 const App = () => (
   <div className="app">
     <Bandeau />
     <Header />
-  <Slider />
+    <Slider />
   </div>
 );
 
--- Front/src/components/Slider/index.js
+++ Front/src/components/Slider/index.js
@@ -4,7 +4,7 @@ import React from 'react';
 import './slider.scss';
 
 const Slider = () => (
-  <div class="slider">
+  <div className="slider">
     <div className="slider__btn--gauche">
     <div className="slider__btn--logo">+</div>
     </div>
