735756c5b64e8bc496315188dcccb9dd396c29c0
PiMa31 <pierre.masset@bbox.fr>
2021-02-11 09:50:57 +0100

    création bandeau

---
+++ Front/src/components/Bandeau/index.js
@@ -0,0 +1,20 @@
+import React from 'react';
+
+import PropTypes from 'prop-types';
+
+import './bandeau.scss';
+
+const Bandeau = () => (
+  <div className="bandeau">
+    <div className="bandeau__timer">Timer boutique : 2 jours 4 heures 23 minutes</div>
+    <form className="bandeau__form">
+      <input className="bandeau_input--email" placeholder="Votre email"></input>
+      <input className="bandeau_input--password" placeholder="Votre mot de passe"></input>
+      <button>OK</button>
+      <a href="#">Créer un compte</a>
+    </form>
+  </div>
+
+)
+
+export default Bandeau;
