6f533843a5f1209e35c2d8256a0cd2d8283e03d3
BenoitLetessier <ciib@free.fr>
2021-02-11 13:12:57 +0100

    Front BL Slider V1 stable

--- Front/src/components/Slider/index.js
+++ Front/src/components/Slider/index.js
@@ -3,10 +3,17 @@ import React from 'react';
 
 import './slider.scss';
 
-const Form = () => (
-  <div className="slider">test</div>
+const Slider = () => (
+  <div class="slider">
+    <div className="slider__btn--gauche">
+    <div className="slider__btn--logo">+</div>
+    </div>
+    <div className="slider__center">centre</div>
+    <div className="slider__btn--gauche">
+    <div className="slider__btn--logo">+</div>
+    </div>
+  </div>
 );
-
 
 
 export default Slider;
