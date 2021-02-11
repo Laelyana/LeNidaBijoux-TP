f16a0da0b3c14fb038822c8862881a918efbebac
BenoitLetessier <ciib@free.fr>
2021-02-11 14:40:52 +0100

    Front BL modification des class main

--- Front/src/components/Main/index.js
+++ Front/src/components/Main/index.js
@@ -4,38 +4,38 @@ import React from 'react';
 import './main.scss';
 
 const Main = () => (
-  <div className="main">
+  <div classNameNameName="main">
 
-  <div class="card">
-    <div class="card__img bijou1"></div>
-    <div class="card__infos">
-    <h1 class="card__infos--title">Titre Card</h1>
-    <h1 class="card__infos--prix">Prix</h1>
+  <div className="card">
+    <div className="card__img bijou1"></div>
+    <div className="card__infos">
+    <h1 className="card__infos--title">Titre Card</h1>
+    <h1 className="card__infos--prix">Prix</h1>
 </div>
-<p class="card__infos--txt">Lorem ipsum dolor </p>
-<div class="card__link">
+<p className="card__infos--txt">Lorem ipsum dolor </p>
+<div className="card__link">
 </div>
 </div> 
 
-<div class="card">
-    <div class="card__img bijou2"></div>
-    <div class="card__infos">
-    <h1 class="card__infos--title">Titre Card</h1>
-    <h1 class="card__infos--prix">Prix</h1>
+<div className="card">
+    <div className="card__img bijou2"></div>
+    <div className="card__infos">
+    <h1 className="card__infos--title">Titre Card</h1>
+    <h1 className="card__infos--prix">Prix</h1>
 </div>
-<p class="card__infos--txt">Lorem ipsum dolor </p>
-<div class="card__link">
+<p className="card__infos--txt">Lorem ipsum dolor </p>
+<div className="card__link">
 </div>
 </div> 
 
-<div class="card">
-    <div class="card__img bijou3"></div>
-    <div class="card__infos">
-    <h1 class="card__infos--title">Titre Card</h1>
-    <h1 class="card__infos--prix">Prix</h1>
+<div className="card">
+    <div className="card__img bijou3"></div>
+    <div className="card__infos">
+    <h1 className="card__infos--title">Titre Card</h1>
+    <h1 className="card__infos--prix">Prix</h1>
 </div>
-<p class="card__infos--txt">Lorem ipsum dolor </p>
-<div class="card__link">
+<p className="card__infos--txt">Lorem ipsum dolor </p>
+<div className="card__link">
 </div>
 </div> 
 
