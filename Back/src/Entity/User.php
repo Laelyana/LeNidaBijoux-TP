d9d30c05f082e1a5e1cbd6747eac2bbe3844ec5b
Nicolas-PAIRON <niquaula@gmail.com>
2021-02-12 09:56:39 +0100

    fix Category in Product + add createAt in construct

--- Back/src/Entity/User.php
+++ Back/src/Entity/User.php
@@ -3,6 +3,7 @@
 namespace App\Entity;
 
 use App\Repository\UserRepository;
+use DateTime;
 use Doctrine\Common\Collections\ArrayCollection;
 use Doctrine\Common\Collections\Collection;
 use Doctrine\ORM\Mapping as ORM;
@@ -73,6 +74,7 @@ class User
     {
         $this->orders = new ArrayCollection();
         $this->product = new ArrayCollection();
+        $this->createdAt = new DateTime();
     }
 
     public function getId(): ?int
