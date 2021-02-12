580340b203c3829c0eb77a98def99b7e9229dfbd
Lauriane G <lauriane.g18@gmail.com>
2021-02-12 10:05:20 +0100

    faker install

--- Back/src/DataFixtures/AppFixtures.php
+++ Back/src/DataFixtures/AppFixtures.php
@@ -2,16 +2,27 @@
 
 namespace App\DataFixtures;
 
+use Faker;
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Persistence\ObjectManager;
 use App\Entity\Product;
 use App\Entity\Style;
 use App\Entity\Category;
+use Symfony\Component\Security\Core\Encoder\UserPasswordEncoderInterface;
 
 class AppFixtures extends Fixture
 {
+
+    public function __construct(UserPasswordEncoderInterface $encoder)
+    {
+        $this->encoder = $encoder;
+    }
+
     public function load(ObjectManager $manager)
     {
+        // On crée une instance de Faker en français
+        $generator = Faker\Factory::create('fr_FR');
+
         $category = New Category();
 
         $category->setName('plouf');
