51b179c2766081508b0348077fdf153d76e8ea60
PiMa31 <pierre.masset@bbox.fr>
2021-02-12 12:27:26 +0100

    css legals mentions

--- Front/src/components/Presentation/index.js
+++ Front/src/components/Presentation/index.js
@@ -9,12 +9,22 @@ const Presentation = () => (
   <div className="presentation">
     <div className="presentation__article">
       <img src={presentation1} alt="#" />
-      <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Distinctio similique ab laboriosam explicabo velit ex, non
+      <div className="presentation__article--p">
+        <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Distinctio similique ab laboriosam explicabo velit ex, non
 	rerum delectus quod aliquid ut, minus inventore earum dolorum magnam voluptatibus, pariatur accusantium? Eligendi.</p>
+        <br></br><br></br>
+        <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Distinctio similique ab laboriosam explicabo velit ex, non
+	rerum delectus quod aliquid ut, minus inventore earum dolorum magnam voluptatibus, pariatur accusantium? Eligendi.</p>
+      </div>
     </div>
     <div className="presentation__article">
-      <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Distinctio similique ab laboriosam explicabo velit ex, non
+      <div className="presentation__article--p">
+        <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Distinctio similique ab laboriosam explicabo velit ex, non
 	rerum delectus quod aliquid ut, minus inventore earum dolorum magnam voluptatibus, pariatur accusantium? Eligendi.</p>
+        <br></br><br></br>
+        <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Distinctio similique ab laboriosam explicabo velit ex, non
+	rerum delectus quod aliquid ut, minus inventore earum dolorum magnam voluptatibus, pariatur accusantium? Eligendi.</p>
+      </div>
       <img src={presentation2} alt="#" />
     </div>
   </div>
