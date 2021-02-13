ea7da0f153a95193bf4ebd94d04c45d205e51f9b
BenoitLetessier <ciib@free.fr>
2021-02-13 10:09:00 +0100

    Front BL mise en place products

---
+++ Front/src/components/Products/index.js
@@ -0,0 +1,19 @@
+import React from 'react';
+
+
+import './Products.scss';
+
+const Products = () => (
+  <div className="products">
+
+<div className="prod prod__img prod__img--un">
+ <div className="prod__center">
+   <div className="prod__center--title">Titre du produit</div>
+ </div>
+</div> 
+
+</div>
+);
+
+
+export default Products;
