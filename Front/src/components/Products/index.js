2ae10ec9963bed5d59ba7bac79ddc0adc1c508dc
BenoitLetessier <ciib@free.fr>
2021-02-13 10:41:13 +0100

    Front BL listes des blocs produits stable

--- Front/src/components/Products/index.js
+++ Front/src/components/Products/index.js
@@ -7,8 +7,20 @@ const Products = () => (
   <div className="products">
 
 <div className="prod prod__img prod__img--un">
- <div className="prod__center">
-   <div className="prod__center--title">Titre du produit</div>
+ <div className="prod__band">
+   <div className="prod__band--title">PRODUIT 1</div>
+ </div>
+</div> 
+
+<div className="prod prod__img prod__img--deux">
+ <div className="prod__band">
+   <div className="prod__band--title">PRODUIT 2</div>
+ </div>
+</div> 
+
+<div className="prod prod__img prod__img--trois">
+ <div className="prod__band">
+   <div className="prod__band--title">PRODUIT 3</div>
  </div>
 </div> 
 
