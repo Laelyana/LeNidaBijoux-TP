f795059fe93b8875ba49433f354b32c64045b875
Lauriane G <lauriane.g18@gmail.com>
2021-02-15 15:36:14 +0100

    collections list - api

--- Back/src/Entity/Colection.php
+++ Back/src/Entity/Colection.php
@@ -7,6 +7,7 @@ use DateTime;
 use Doctrine\Common\Collections\ArrayCollection;
 use Doctrine\Common\Collections\Collection;
 use Doctrine\ORM\Mapping as ORM;
+use Symfony\Component\Serializer\Annotation\Ignore;
 
 /**
  * @ORM\Entity(repositoryClass=ColectionRepository::class)
@@ -42,6 +43,7 @@ class Colection
 
     /**
      * @ORM\OneToMany(targetEntity=Product::class, mappedBy="colection")
+     * @Ignore()
      */
     private $products;
 
