29490adf7c3c738106100ae8c54c5f5fb089cbc6
BenoitLetessier <ciib@free.fr>
2021-02-15 10:36:06 +0100

    Front BL fiche produit img stable

--- Front/src/components/Product/index.js
+++ Front/src/components/Product/index.js
@@ -6,14 +6,20 @@ import './Product.scss';
 const Product = () => (
   <div className="product">
 <div className="product__box__img">
-<div className="product__box__img--un">img big</div>
-<div className="product__box__img--unBis">img little 1</div>
-<div className="product__box__img--deux">img little 2</div>
-<div className="product__box__img--trois">img little 3</div>
+<div className="product__box__img--un image__un"></div>
+<div className="product__box__img__sub--unBis image__un"></div>
+<div className="product__box__img__sub--deux image__deux"></div>
+<div className="product__box__img__sub--trois image__trois"></div>
 </div>
 <div className="product__box__txt">
   <div className="product__box__txt--title">Titre de l'article</div>
+  <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Distinctio similique ab laboriosam explicabo velit ex, non rerum delectus quod aliquid ut, minus inventore earum dolorum magnam voluptatibus, pariatur accusantium? Eligendi.
+Lorem ipsum dolor sit amet consectetur, adipisicing elit. Distinctio similique ab laboriosam explicabo velit ex, non rerum delectus quod aliquid ut, minus inventore earum dolorum magnam voluptatibus, pariatur accusantium? Eligendi.</p>
 </div>
+<div className="product__box__cart">
+  <div className="product__box__cart--price">40$</div>
+  
+  </div>
 </div>
 );
 
