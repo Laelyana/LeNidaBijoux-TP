3f3a34a5bcd95f36b70c8fe9ae485b91a1c257cd
BenoitLetessier <ciib@free.fr>
2021-02-16 10:53:43 +0100

    Front BL slide image produit au clic et survol OK

--- Front/src/components/Product/index.js
+++ Front/src/components/Product/index.js
@@ -3,71 +3,30 @@ import React from 'react';
 
 import './Product.scss';
 
+function handleClick__un() {
+  let general = document.getElementById("general");
+  general.className = "product__box__img--un image__un";
+}
 
-// let general = document.getElementByCla("general");
-// let bis = document.getElementById("bis");
-// let deux = document.getElementById("deux");
-// let trois = document.getElementById("trois");
+function handleClick__deux() {
+  let general = document.getElementById("general");
+  general.className = "product__box__img--un image__deux";
+}
 
-// bis.addEventListener("click", bisF);
-// deux.addEventListener("click", deuxF);
-// trois.addEventListener("click", troisF);
-  
-//   function bisF() {
-//     general.className = bis;
-//     console.log("click");
-//   };
-  
-//   function deuxF() {
-//     general.className = deux;
-//   };
-  
-//   function troisF() {
-//     general.className = trois;
-//   };
-
-// function handleClick() {
-
-// let general = document.getElementsByClassName("product__box__img--un image__un");
-// let bis = document.getElementsByClassName("product__box__img--un image__un");
-// let deux = document.getElementsByClassName("product__box__img--un image__deux");
-// let trois = document.getElementsByClassName("product__box__img--un image__trois");
-
-// bis.addEventListener("click", bisF);
-// deux.addEventListener("click", deuxF);
-// trois.addEventListener("click", troisF);
-  
-//   function bisF() {
-//     general.className = bis;
-//     console.log("click bis");
-//   };
-  
-//   function deuxF() {
-//     general.className = deux;
-//     console.log("clic deux");
-//   };
-  
-//   function troisF() {
-//     general.className = trois;
-//     console.log("clic trois");
-//   };
-// }
-
-function ActionLink() {
-  function handleClick(e) {
-    e.preventDefault();
-    console.log('Le lien a été cliqué.');
-  }
-
+function handleClick__trois() {
+  let general = document.getElementById("general");
+  general.className = "product__box__img--un image__trois";
+}
 
 const Product = () => (
+
 <div className="product">
 <div className="product__box__img">
 <div className="product__box__img--un image__un" id="general"></div>
 <div className="product__box__img__sub">
-<div className="product__box__img__sub--unBis image__un" id="bis" onClick={this.handleClick}></div>
-<div className="product__box__img__sub--deux image__deux" id="deux" onClick={this.handleClick}></div>
-<div className="product__box__img__sub--trois image__trois" id="trois" onClick={this.handleClick}></div>
+<div className="product__box__img__sub--unBis image__un" id="bis" onClick={handleClick__un} onMouseOver={handleClick__un}></div>
+<div className="product__box__img__sub--deux image__deux" id="deux" onClick={handleClick__deux} onMouseOver={handleClick__deux}></div>
+<div className="product__box__img__sub--trois image__trois" id="trois" onClick={handleClick__trois} onMouseOver={handleClick__trois}></div>
 </div>
 </div>
 <div className="product__box__txt">
