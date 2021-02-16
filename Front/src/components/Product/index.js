c9c4ea59f9ea84564570aff8987e95992f4621ba
BenoitLetessier <ciib@free.fr>
2021-02-16 09:56:13 +0100

    Front BL mise a jour branche

--- Front/src/components/Product/index.js
+++ Front/src/components/Product/index.js
@@ -4,7 +4,7 @@ import React from 'react';
 import './Product.scss';
 
 
-// let general = document.getElementById("general");
+// let general = document.getElementByCla("general");
 // let bis = document.getElementById("bis");
 // let deux = document.getElementById("deux");
 // let trois = document.getElementById("trois");
@@ -26,6 +26,38 @@ import './Product.scss';
 //     general.className = trois;
 //   };
 
+// function handleClick() {
+
+// let general = document.getElementsByClassName("product__box__img--un image__un");
+// let bis = document.getElementsByClassName("product__box__img--un image__un");
+// let deux = document.getElementsByClassName("product__box__img--un image__deux");
+// let trois = document.getElementsByClassName("product__box__img--un image__trois");
+
+// bis.addEventListener("click", bisF);
+// deux.addEventListener("click", deuxF);
+// trois.addEventListener("click", troisF);
+  
+//   function bisF() {
+//     general.className = bis;
+//     console.log("click bis");
+//   };
+  
+//   function deuxF() {
+//     general.className = deux;
+//     console.log("clic deux");
+//   };
+  
+//   function troisF() {
+//     general.className = trois;
+//     console.log("clic trois");
+//   };
+// }
+
+function ActionLink() {
+  function handleClick(e) {
+    e.preventDefault();
+    console.log('Le lien a été cliqué.');
+  }
 
 
 const Product = () => (
@@ -33,9 +65,9 @@ const Product = () => (
 <div className="product__box__img">
 <div className="product__box__img--un image__un" id="general"></div>
 <div className="product__box__img__sub">
-<div className="product__box__img__sub--unBis image__un" id="bis"></div>
-<div className="product__box__img__sub--deux image__deux" id="deux"></div>
-<div className="product__box__img__sub--trois image__trois" id="trois" ></div>
+<div className="product__box__img__sub--unBis image__un" id="bis" onClick={this.handleClick}></div>
+<div className="product__box__img__sub--deux image__deux" id="deux" onClick={this.handleClick}></div>
+<div className="product__box__img__sub--trois image__trois" id="trois" onClick={this.handleClick}></div>
 </div>
 </div>
 <div className="product__box__txt">
@@ -45,7 +77,7 @@ Lorem ipsum dolor sit amet consectetur, adipisicing elit. Distinctio similique a
 </div>
 <div className="product__box__cart">
   <div className="product__box__cart--price">40$</div>
-  <input className="product__box__cart--number" type="number" id="nomber" name="nomber" min="0" max="100" placeholder="Quantité"></input>
+  <input className="product__box__cart--number" type="number" id="number" name="number" min="0" max="100" placeholder="Quantité"></input>
   <button className="product__box__cart--btn-add">Ajouter au panier</button>
   <button className="product__box__cart--btn-fav">Ajouter aux favoris</button>
   
