78f6ba47e4e416ad11e39e3697742708c533e447
BenoitLetessier <ciib@free.fr>
2021-02-16 14:24:49 +0100

    Front BL mise a jour de la branche avec recadrage des bloc lien

--- Front/src/components/Products/index.js
+++ Front/src/components/Products/index.js
@@ -6,23 +6,25 @@ import './Products.scss';
 const Products = () => (
   <div className="products">
 
-    <a href="/produit"><div className="prod prod__img prod__img--un">
+    <div className="prod prod__img prod__img--un">
       <div className="prod__band">
-        <div className="prod__band--title">PRODUIT 1</div>
+        <div className="prod__band--title"><a href="/produit">PRODUIT 1</a></div>
       </div>
-    </div></a>
+    
+    </div>
 
-    <a href="/produit"><div className="prod prod__img prod__img--deux">
+    <div className="prod prod__img prod__img--deux">
       <div className="prod__band">
-        <div className="prod__band--title">PRODUIT 2</div>
+        <div className="prod__band--title"><a href="/produit">PRODUIT 2</a></div>
       </div>
-    </div></a>
+    </div>
 
-    <a href="/produit"><div className="prod prod__img prod__img--trois">
+    <div className="prod prod__img prod__img--trois">
+    
       <div className="prod__band">
-        <div className="prod__band--title">PRODUIT 3</div>
+        <div className="prod__band--title"><a href="/produit">PRODUIT 3</a></div>
       </div>
-    </div></a>
+    </div>
 
   </div>
 );
