f8d3189ed67d39465e30727bd554a7fe50bc6f34
PiMa31 <pierre.masset@bbox.fr>
2021-02-17 16:34:54 +0100

    test

--- Front/src/components/Contact/index.js
+++ Front/src/components/Contact/index.js
@@ -1,16 +1,16 @@
 import React from 'react';
 
-import './contact.scss'
+import './contact.scss';
 
 const Contact = () => (
-    <form className="contact__form">
-      <h3>Formulaire de contact</h3>
-      <img src="#" alt="#"/>
-      <input className="contact__form--input" placeholder="Nom / Prénom"></input>
-      <input className="contact__form--input" placeholder="Sujet"></input>
-      <textarea className="contact__form--textarea" placeholder="Descriptif de votre demande"></textarea>
-      <button className="contact__form--button">Envoyer</button>
-    </form>
-)
+  <form className="contact__form">
+    <h3>Formulaire de contact</h3>
+    <img src="#" alt="#" />
+    <input className="contact__form--input" placeholder="Nom / Prénom"></input>
+    <input className="contact__form--input" placeholder="Sujet"></input>
+    <textarea className="contact__form--textarea" placeholder="Descriptif de votre demande"></textarea>
+    <button className="contact__form--button">Envoyer</button>
+  </form>
+);
 
 export default Contact;
--- Front/src/middlewares/favorites.js
+++ Front/src/middlewares/favorites.js
@@ -5,13 +5,7 @@ import { saveFavorites, FETCH_FAVORITES } from '../actions/favorites';
 export default (store) => (next) => (action) => {
   switch (action.type) {
     case FETCH_FAVORITES: {
-      const { token } = store.getState().user;
-      axios.get('http://0.0.0.0:8000/api/favorites/users/4',
-        {
-          headers: {
-            Authorization: `Bearer ${token}`,
-          },
-        })
+      axios.get('http://0.0.0.0:8000/api/categories')
         .then((response) => {
           console.log('coucou');
           store.dispatch(saveFavorites(response.data));
