c6d30ea9d680827a1426492610bbc38485c4761c
BenoitLetessier <ciib@free.fr>
2021-02-17 19:07:43 +0100

    Front BL update and create variables for slider

--- Front/src/components/Slider/index.js
+++ Front/src/components/Slider/index.js
@@ -3,48 +3,36 @@ import React from 'react';
 import un from './img/1.jpg';
 import goldengate from './img/goldengate.jpg';
 
-
 import './slider.scss';
 
-//variable avec class du bouton gauche
-let btnLeft = document.querySelector(".slider__gauche");
-//variable avec class du bouton droit
-let btnRight = document.querySelector(".slider__droite");
-//variable du slider general
-let slider = document.querySelector(".slider");
-//variable item__slider qui contient chaque image
-let sliderItem = document.querySelectorAll(".slider__item");
-//variable de rond pour les btn
-let sliderBtn = document.querySelector(".slider__btn");
-//varable slider list ou sont les slider__item afin de les rassembler, compter et boucler
-let sliderList = document.querySelector("#slider__list");
-//on compte le nombre de class slider__item dans le bloc slider__list
-let nombre = document.querySelectorAll('#slider__list > .slider__item').length;
+//vanilla Slider
 
-//console.log(nombre);
+//on stock les class
+let timerSlide = '2500'; //timer
+let slider__list = document.querySelector('#slider'); //la div parent qui contient les div photos
+let slider__items = document.querySelectorAll('.slider__item'); //la div enfant, la variables sélectionne toutes les class du même nom
+let sliderItemNumber = slider__items.length; //nombre de divs slider__item
+let btnGauche = document.querySelector('.slider__gauche'); //btn gauche
+let btnDroit = document.querySelector('.slider__droite'); //btn droit
 
-function gauche(){
-alert("clic btn gauche CURIEUX !")
-};
 
-function droit(){
-  alert("clic btn droit CURIEUX !")
-  };
+
 
 const Slider = () => (
-
+   
   <div className="slider">
   <div className="slider__btn--haut" ></div>
   <div id="slider__list">
-  <div className="slider__item"><img className="slider__img" src={un} alt="" /></div>
-  <div className="slider__item"><img className="slider__img" src={goldengate} alt="" /></div>
+  <div className="slider__item active" id="1"><img className="slider__img" src={un} alt="" /></div>
+  <div className="slider__item" id="2"><img className="slider__img" src={goldengate} alt="" /></div>
   </div>
-  <div className="slider__gauche" onClick={gauche}>
-  <div className="slider__btn" ></div>
-  </div>
-  <div className="slider__droite" onClick={droit}>
+  <div className="slider__gauche">
   <div className="slider__btn"></div>
   </div>
+  <div className="slider__droite">
+  <div className="slider__btn"></div>
+  </div>
+  
 </div>
 );
 
