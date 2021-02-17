523830d4d68c17e59787a5ed903dfecba58ab94c
BenoitLetessier <ciib@free.fr>
2021-02-17 16:08:01 +0100

    Front BL Slider update scss jsx for api link

--- Front/src/components/Slider/index.js
+++ Front/src/components/Slider/index.js
@@ -1,19 +1,25 @@
 import React from 'react';
 
+import goldengate from './img/goldengate.jpg';
 
 import './slider.scss';
 
+
 const Slider = () => (
+
   <div className="slider">
-    <div className="slider__btn--gauche">
-    <div className="slider__btn--logo">+</div>
-    </div>
-    <div className="slider__center">centre</div>
-    <div className="slider__btn--gauche">
-    <div className="slider__btn--logo">+</div>
-    </div>
+  <div className="slider__btn--bas"></div>
+  <div className="slider__item"><img className="slider__img" src={goldengate} alt="" /></div>
+  <div className="slider__gauche">
+  <div className="slider__btn"></div>
   </div>
+  <div className="slider__droite">
+  <div className="slider__btn"></div>
+  </div>
+</div>
 );
 
 
 export default Slider;
+
+
