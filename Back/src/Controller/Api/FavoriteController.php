19ef427ab9bb334d8b18f85f698b1eb81881ad44
Nicolas-PAIRON <niquaula@gmail.com>
2021-02-17 14:24:32 +0100

    create addProductToUser and deleteProductFromUser methods + handle errors code

--- Back/src/Controller/Api/FavoriteController.php
+++ Back/src/Controller/Api/FavoriteController.php
@@ -2,7 +2,7 @@
 
 namespace App\Controller\Api;
 
-use App\Entity\User;
+use App\Repository\ProductRepository;
 use App\Repository\UserRepository;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
@@ -16,21 +16,71 @@ class FavoriteController extends AbstractController
     /**
      * @Route("/api/favorites/users/{id}", name="api_favorites_browse_by_user", methods={"GET"})
      */
-    public function browseByUser(User $user): Response
+    public function browseByUser(UserRepository $userRepo, int $id): Response
     {
-        return $this->json($user->getProduct());
+        if($user = $userRepo->find($id)){
+
+            return $this->json($user->getProduct(), 200);
+
+        }else{
+
+            return $this->json("this user doesn't exist", 404);
+        }
     }
 
     /**
-     * @Route("/api/favorites/users/{id}", name="api_favorites_update_by_user", methods={"POST"})
+     * @Route("/api/favorites/users/{id}", name="api_favorites_add_by_user", methods={"PATCH"})
      */
-    public function addProductToUser(Request $request, EntityManagerInterface $em, User $user): Response
+    public function addProductToUser(int $id, Request $request, EntityManagerInterface $em, UserRepository $userRepo, ProductRepository $productRepo): Response
     {
-        $infoRequestAsArray = json_decode($request->getContent(), true);
-        $user->addProduct($infoRequestAsArray);
-        $em->persist($user);
-        $em->flush();
+        if($user = $userRepo->find($id)){
 
-        return $this->json($user, 201);
+            $infoRequest = json_decode($request->getContent(), false);
+
+            if($infoRequest != null && property_exists ($infoRequest , "productId")){
+
+                if($product = $productRepo->find($infoRequest->productId)){
+        
+                    $user->addProduct($product);
+                    $em->persist($user);
+                    $em->flush();
+                    return $this->json($user->getProduct(), 200);
+        
+                }else{
+        
+                    return $this->json("this product doesn't exist", 404);
+                }
+
+            }else{
+
+                return $this->json("bad request", 400);
+            }
+
+        }else{
+
+            return $this->json("this user doesn't exist", 404);
+        }
+    }
+
+    /**
+     * @Route("/api/favorites/{product_id}/users/{user_id}", name="api_favorites_delete_by_user", methods={"DELETE"})
+     */
+    public function deleteProductFromUser(int $product_id, int $user_id, ProductRepository $productRepo, UserRepository $userRepo, EntityManagerInterface $em): Response
+    {
+        if($user = $userRepo->find($user_id)){
+
+            if($product = $productRepo->find($product_id)){
+    
+                $user->removeProduct($product);
+                $em->persist($user);
+                $em->flush();
+            }
+
+            return $this->json(null, 204);
+
+        }else{
+
+            return $this->json("this user doesn't exist", 404);
+        }
     }
 }
