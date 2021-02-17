0a1ac05b3191b8567ca0cdf7da676e8093ae8a28
BenoitLetessier <ciib@free.fr>
2021-02-17 16:38:11 +0100

    Front BL Slider update nav top

--- Front/src/components/Slider/index.js
+++ Front/src/components/Slider/index.js
@@ -1,14 +1,24 @@
 import React from 'react';
 
+import un from './img/1.jpg';
 import goldengate from './img/goldengate.jpg';
 
+
 import './slider.scss';
+
+let btnLeft = document.querySelector(".slider__gauche");
+let btnRight = document.querySelector(".slider__droite");
+let slider = document.querySelector(".slider");
+let sliderItem = document.querySelector(".slider__item");
+let sliderBtn = document.querySelector(".slider__btn");
+
 
 
 const Slider = () => (
 
   <div className="slider">
-  <div className="slider__btn--bas"></div>
+  <div className="slider__btn--haut" ></div>
+  <div className="slider__item"><img className="slider__img" src={un} alt="" /></div>
   <div className="slider__item"><img className="slider__img" src={goldengate} alt="" /></div>
   <div className="slider__gauche">
   <div className="slider__btn"></div>
