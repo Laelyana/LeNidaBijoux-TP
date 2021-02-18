a84b177684a00248956419d5871505064318cf1f
Nicolas-PAIRON <niquaula@gmail.com>
2021-02-18 10:40:37 +0100

    route api_carts done

--- Back/src/Controller/Api/CartController.php
+++ Back/src/Controller/Api/CartController.php
@@ -17,26 +17,43 @@ use Symfony\Component\Routing\Annotation\Route;
 class CartController extends AbstractController
 {
     /**
-     * @Route("/api/carts/user/{id}", name="api_carts", methods={"POST"})
+     * @Route("/api/carts/users/{id}", name="api_carts", methods={"POST"})
      */
     public function add(int $id, OrderRepository $orderRepo, ProductRepository $productRepo, UserRepository $userRepo, Request $request, EntityManagerInterface $em): Response
     {
         if($user = $userRepo->find($id)){
 
+            $infoFromClientAsArray = json_decode($request->getContent(), false);
+
+            $errors=[];
+
+            foreach ($infoFromClientAsArray as $cart) {
+                $productId = $cart->productId;
+                $quantity = $cart->quantity;
+                
+                if (! $product = $productRepo->find($productId)) {
+                    $errors[]="product doesn't exist";
+                }
+
+                if (! is_int($quantity) || $quantity<0) {
+                    $errors[]="the quantity is not valid";
+                }
+            }
+
+            if(count($errors) > 0){
+                return $this->json($errors, 400);
+                
+            }
+
             $order = new Order();
             $order->setUser($user);
             $em->persist($order);
             $em->flush();
-            //dd($order);
-
-            $infoFromClientAsArray = json_decode($request->getContent(), false);
-            //dump($infoFromClientAsArray);
-
+            
             foreach($infoFromClientAsArray as $cart){
 
                 $productId = $cart->productId;
                 $quantity = $cart->quantity;
-
                 $product = $productRepo->find($productId);
 
                 $orderLine = new OrderLine();
@@ -46,12 +63,11 @@ class CartController extends AbstractController
                 $orderLine->setOrderEntity($order);
                 
                 $em->persist($orderLine);
-             
             }
 
             $em->flush();
             
-        return $this->json($order, 200);
+            return $this->json($order, 200);
 
         }else{
 
