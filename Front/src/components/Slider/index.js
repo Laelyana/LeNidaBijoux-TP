8306afee92fc2826a9930a7fe316f1126fe28cab
BenoitLetessier <ciib@free.fr>
2021-02-18 11:36:25 +0100

    Front BL Slider update function click left and right

--- Front/src/components/Slider/index.js
+++ Front/src/components/Slider/index.js
@@ -4,12 +4,34 @@ import Slide from './slide';
 
 import './slider.scss';
 
+
 const Slider = ({ manageLoad, slider }) => {
   useEffect(
     manageLoad,
     [],
   );
+
+//code vanilla slider
+let timerSlide = '2500'; //timer
+let slider__list = document.querySelector('#slider'); //la div parent qui contient les div photos
+let slider__items = document.querySelectorAll('.slider__item'); //la div enfant, la variables sélectionne toutes les class du même nom
+let sliderItemNumber = slider__items.length; //nombre de divs slider__item
+let btnGauche = document.querySelector('.slider__gauche'); //btn gauche
+let btnDroit = document.querySelector('.slider__droite'); //btn droit
+
+function gauche() {
+console.log("clic gauche");
+};
+
+function droite() {
+  console.log("clic droite");
+  };
+
   return (
+
+    <div className="slider">
+    <div className="slider__btn--haut" ></div>
+    <div id="slider__list">
     <div>
       {
         slider.map((slide) => (
@@ -18,6 +40,17 @@ const Slider = ({ manageLoad, slider }) => {
         ))
       }
     </div>
+    </div>
+    <div className="slider__gauche" onClick={gauche}>
+    <div className="slider__btn"></div>
+    </div>
+    <div className="slider__droite" onClick={droite}>
+    <div className="slider__btn"></div>
+    </div>
+    
+  </div>
+
+ 
   );
 };
 
