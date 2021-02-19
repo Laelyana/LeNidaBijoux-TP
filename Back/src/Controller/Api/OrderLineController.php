39b7537da5463a6d87e5835c188f0b4d3286b9f4
Nicolas-PAIRON <niquaula@gmail.com>
2021-02-19 13:54:11 +0100

    modify code error orderlines route

--- Back/src/Controller/Api/OrderLineController.php
+++ Back/src/Controller/Api/OrderLineController.php
@@ -2,14 +2,9 @@
 
 namespace App\Controller\Api;
 
-use App\Entity\User;
 use App\Repository\OrderLineRepository;
 use App\Repository\OrderRepository;
-use App\Repository\ProductRepository;
-use App\Repository\UserRepository;
-use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
-use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
@@ -19,8 +14,15 @@ class OrderLineController extends AbstractController
     /**
      * @Route("/api/order-lines/orders/{id}", name="api_order_lines_browse_by_order", methods={"GET"})
      */
-    public function browseByOrder(OrderLineRepository $orderLineRepo, int $id): Response
+    public function browseByOrder(OrderLineRepository $orderLineRepo, int $id, OrderRepository $orderRepo): Response
     {
-        return $this->json($orderLineRepo->findBy(['orderEntity' => $id]), 200);
+        if($orderRepo->find($id)){
+
+            return $this->json($orderLineRepo->findBy(['orderEntity' => $id]), 200);
+
+        }else{
+
+            return $this->json("this order doesn't exist", 404);
+        }
     }
 }
