d4f56a102577258278d98a2fce863835d9d1ac2c
PiMa31 <pierre.masset@bbox.fr>
2021-02-19 13:55:16 +0100

    nothing

--- Front/src/actions/user.js
+++ Front/src/actions/user.js
@@ -6,6 +6,7 @@ export const CREATE_USER = 'CREATE_USER';
 export const SAVE_NEW_USER_DATA = 'SAVE_NEW_USER_DATA';
 export const CHANGE_NEW_USER_FIELD = 'CHANGE_NEW_USER_FIELD';
 export const EDIT_USER = 'CREATE_USER';
+export const FETCH_USER_DATA = 'FETCH_USER_DATA';
 
 export const changeUserField = (fieldValue, fieldName) => ({
   type: CHANGE_USER_FIELD,
