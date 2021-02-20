6961223852483205291633ebb4268cd775daea0b
BenoitLetessier <ciib@free.fr>
2021-02-20 18:38:31 +0100

    Front BL footer update with responsive and btn

--- Front/src/components/Footer/index.js
+++ Front/src/components/Footer/index.js
@@ -11,21 +11,19 @@ const Footer = () => (
   <footer className="footer">
     <div className="footer__legals">
       <ul className="footer__legals--ul">
-        <a href="/mentions-legales"><li className="footer__legals--li">Mentions légales</li></a>
-        <a href="conditions-de-vente"><li className="footer__legals--li">Conditions générales de vente</li></a>
+        <li className="footer__legals--li"><a href="/mentions-legales">Mentions légales</a></li>
+        <li className="footer__legals--li"><a href="conditions-de-vente">Conditions générales de vente</a></li>
       </ul>
     </div>
     <div className="footer__social">
-      <div className="footer__social--img">
-        <a href="https://www.facebook.com/lenidabijoux"><img className="socialLogo" src={faecebookLogo} alt="logo FB" /></a>
-        <a href="https://www.instagram.com/le_nid_a_bijoux/"><img className="socialLogo" src={instagramLogo} alt="logo Insta" /></a>
-      </div>
+    <div><a href="https://www.facebook.com/lenidabijoux"><img className="footer__social--logo" src={faecebookLogo} alt="logo FB" /></a></div>    
+    <div><a href="https://www.instagram.com/le_nid_a_bijoux/"><img className="footer__social--logo" src={instagramLogo} alt="logo Insta" /></a></div>
     </div>
     <div className="footer__contact">
       <ul className="footer__contact--ul">
-        <li className="footer_contact--li">1 rue par ici 31000 Parla</li>
-        <li className="footer_contact--li">0123456789</li>
-        <a href="/contactez-moi"><li className="footer_contact--li">Contactez moi</li></a>
+        <li className="footer__contact--li">1 rue par ici 31000 Parla</li>
+        <li className="footer__contact--li">0123456789</li>
+        <li className="footer__contact--li-form"><a href="/contactez-moi">Contactez moi</a></li>
       </ul>
     </div>
   </footer>
