763b29c29df0739ee74386693f263c9bef8912d8
Nicolas-PAIRON <niquaula@gmail.com>
2021-02-23 16:26:22 +0100

    fix required in imageField on edit

--- Back/src/Controller/Admin/SliderCrudController.php
+++ Back/src/Controller/Admin/SliderCrudController.php
@@ -3,6 +3,7 @@
 namespace App\Controller\Admin;
 
 use App\Entity\Slider;
+use EasyCorp\Bundle\EasyAdminBundle\Config\Crud;
 use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractCrudController;
 use EasyCorp\Bundle\EasyAdminBundle\Field\ChoiceField;
 use EasyCorp\Bundle\EasyAdminBundle\Field\ImageField;
@@ -17,8 +18,14 @@ class SliderCrudController extends AbstractCrudController
     
     public function configureFields(string $pageName): iterable
     {
+        if($pageName === Crud::PAGE_EDIT){
+            $required = false;
+        }else{
+            $required = true;
+        }
+        
         return [
-            ImageField::new('picture','Image')->setUploadDir('public/pictures')->setBasePath('/pictures'),
+            ImageField::new('picture','Image')->setUploadDir('public/pictures')->setBasePath('/pictures')->setRequired($required),
             'active'
         ];
     }
