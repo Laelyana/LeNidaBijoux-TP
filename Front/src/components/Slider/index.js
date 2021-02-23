690437f9533405a8a1147b234fa4f4ae8acb5931
BenoitLetessier <ciib@free.fr>
2021-02-23 10:10:42 +0100

    Front BL Slider clean function

--- Front/src/components/Slider/index.js
+++ Front/src/components/Slider/index.js
@@ -9,33 +9,6 @@ import './slider.scss';
 //let SliderGenerale = addEventListener('click', onClickSlider, true);
 
 
-function onClickSlider(e) {
-  e.stopPropagation();
- let slides = document.querySelectorAll(".slider__img");
-//console.log(slides);
- let section = document.querySelector("section");
- //console.log(section);
- let t = e.target;
- let currImage = 0;
- 
- if (t.tagName.toUpperCase() != 'BUTTON')
-		return;
- 
- if (t.classList.contains('slider__next')) {
-   currImage++;
-   section.className = "slider__img slider__img--active";
-   //console.log("clic ++");
-
- }
-
- else {
-   currImage--;
-   //console.log("clic --");
-   currImage = currImage.length-1
-   section.className = "slider__img slider__img";
- }
-
-}
 
 
 const Slider = ({ manageLoad, slider }) => {
@@ -43,45 +16,7 @@ const Slider = ({ manageLoad, slider }) => {
     manageLoad,
     [],
   );
-  console.log(slider);
-
-  // code vanilla slider
-  // let children  = document.querySelectorAll("slider__img");
-  // console.log(children);
-  let carousel = document.querySelector("slider__list");
-
-  //let children = figure.childElementCount;
-  let nav = document.querySelector('nav');
-  //let numImages = figure.childElementCount;
-
-  let currImage = 0;
-
-  window.addEventListener('click', onClick, true);
-
-  function onClick(e) {
-    e.stopPropagation();
-    //let numImages = figure.childElementCount;
-    let amount = document.querySelectorAll(".slide__img").length;
-    console.log(amount);
-    let figure = document.querySelectorAll(".slide__item");
-    let children = document.querySelectorAll("slide__img");
-    let numImages = figure.childElementCount;
-    let t = e.target;
-    let currImage = 0;
-
-    if (t.tagName.toUpperCase() != 'BUTTON')
-      return;
-
-    if (t.classList.contains('slider__next')) {
-      currImage++;
-      console.log("clic ++");
-    }
-
-    else {
-      currImage--;
-      console.log("clic --");
-    }
-  }
+  
 
   return (
 
