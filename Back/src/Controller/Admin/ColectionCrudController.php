07080f4afde533f8df38e578645fc171594691b0
Lauriane G <lauriane.g18@gmail.com>
2021-02-24 09:36:05 +0100

    collections pictures added

--- Back/src/Controller/Admin/ColectionCrudController.php
+++ Back/src/Controller/Admin/ColectionCrudController.php
@@ -5,6 +5,7 @@ namespace App\Controller\Admin;
 use App\Entity\Colection;
 use EasyCorp\Bundle\EasyAdminBundle\Config\Crud;
 use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractCrudController;
+use EasyCorp\Bundle\EasyAdminBundle\Field\ImageField;
 use EasyCorp\Bundle\EasyAdminBundle\Field\TextField;
 
 class ColectionCrudController extends AbstractCrudController
@@ -16,8 +17,17 @@ class ColectionCrudController extends AbstractCrudController
 
     public function configureFields(string $pageName): iterable
     {
+        if($pageName === Crud::PAGE_EDIT){
+            $required = false;
+        }else{
+            $required = true;
+        }
+        
         return [
             TextField::new('name','Nom de la collection'),
+            ImageField::new('picture','Image')->setUploadDir('public/pictures')
+                                              ->setBasePath('/pictures')
+                                              ->setRequired($required)
         ];
     }
 
