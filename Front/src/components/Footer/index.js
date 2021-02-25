95b1fbff734f45351f1a746a90e596046dc33091
BenoitLetessier <ciib@free.fr>
2021-02-25 14:49:30 +0100

    Front BL update design page index and cat

--- Front/src/components/Footer/index.js
+++ Front/src/components/Footer/index.js
@@ -11,7 +11,7 @@ const Footer = () => (
   <div className="footer__center">
 
   <div className="footer__center--left">
-      <ul className="footer__legals--ul">
+      <ul>
         <li className="footer__legals--li"><a href="/mentions-legales">Mentions légales</a></li>
         <li className="footer__legals--li"><a href="conditions-de-vente">Conditions générales de vente</a></li>
       </ul>
@@ -20,13 +20,12 @@ const Footer = () => (
     <div className="footer__center--right">
    <div> <a href="https://www.facebook.com/lenidabijoux"><img  src={faecebookLogo} alt="logo FB" className="footer__social--logo"/></a>  </div> 
    <div> <a href="https://www.instagram.com/le_nid_a_bijoux/"><img  src={instagramLogo} alt="logo Insta" className="footer__social--logo" /></a></div>
-    <div className="footer__contact">
       <ul className="footer__contact--ul">
         <li className="footer__contact--li">1 rue par ici 31000 Parla</li>
         <li className="footer__contact--li">0123456789</li>
         <li className="footer__contact--li-form"><a href="/contactez-moi">Contactez moi</a></li>
       </ul>
-    </div>
+   
     </div>
     </div>
   </footer>
