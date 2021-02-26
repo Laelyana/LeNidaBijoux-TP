8ee05b9358b41faf77bd8819421dbf8c058e4e36
BenoitLetessier <ciib@free.fr>
2021-02-26 15:04:32 +0100

    Front BL update dashboard

--- Front/src/components/Dashboard/index.js
+++ Front/src/components/Dashboard/index.js
@@ -6,11 +6,18 @@ const Dashboard = () => (
   <div className="dashboard">
     <h2> Mon tableau de bord</h2>
     <div className="dashboard__items">
-      <a className="dashboard__items--box" href="/mon-compte/mes-infos"><div><h4>Mon profil</h4></div></a>
-      <a className="dashboard__items--box" href="/mon-compte/mes-favoris"><div><h4>Mes favoris</h4></div></a>
-      <a className="dashboard__items--box" href="/mon-compte/mes-commandes"><div><h4>Mes commandes</h4></div></a>
+      <div className="dashboard__items--haut profil"></div>
+      <div className="dashboard__items--bas"><h4>Mon profil</h4></div>
+      </div>
+      <div className="dashboard__items">
+      <div className="dashboard__items--haut favoris"></div>
+      <div className="dashboard__items--bas"><h4>Mes favoris</h4></div>
+      </div>
+      <div className="dashboard__items">
+      <div className="dashboard__items--haut commandes"></div>
+      <div className="dashboard__items--bas"><h4>Mes commandes</h4></div>
+      </div>
     </div>
-  </div>
 );
 
 export default Dashboard;
