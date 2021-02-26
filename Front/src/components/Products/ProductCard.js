75476d4a17f24131a293e9442aaac899d12d6fbf
BenoitLetessier <ciib@free.fr>
2021-02-26 08:48:06 +0100

    Front BL update img products img sizes  ok

--- Front/src/components/Products/ProductCard.js
+++ Front/src/components/Products/ProductCard.js
@@ -9,9 +9,9 @@ const ProductCard = ({ name, id, picture1}) => (
 
   <div className="prod">
         <div className="prod prod__img">
-        <img id={id} className="" src={`http://0.0.0.0:8000/pictures/` + picture1} alt="" />
+        <img id={id} className="prod__img-card" src={`http://0.0.0.0:8000/pictures/` + picture1} alt="" />
           <div className="prod__band">
-            <div className="prod__band--title"><Link to={`/${name}`} />{name}</div>
+            <div className="prod__band--title">{name}</div>
           </div>
         </div>
       </div>
