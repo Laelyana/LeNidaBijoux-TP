da4e993f5f5cabe70b8e3adb62e989cb2b943672
PiMa31 <pierre.masset@bbox.fr>
2021-02-26 09:44:01 +0100

    rien

--- Front/src/components/Products/ProductCard.js
+++ Front/src/components/Products/ProductCard.js
@@ -2,16 +2,14 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import { Link } from 'react-router-dom';
 
-
 import './products.scss';
 
-const ProductCard = ({ name, id, picture1}) => (
+const ProductCard = ({ name, id }) => (
 
   <div className="prod">
-        <div className="prod prod__img">
-        <img id={id} className="prod__img-card" src={`http://0.0.0.0:8000/pictures/` + picture1} alt="" />
+        <div className="prod prod__img prod__img--un">
           <div className="prod__band">
-            <div className="prod__band--title">{name}</div>
+            <div className="prod__band--title"><Link to={`/${name}`} />{name}</div>
           </div>
         </div>
       </div>
