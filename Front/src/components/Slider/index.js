a9253c2e5ac77acd3443e391202edab688770911
PiMa31 <pierre.masset@bbox.fr>
2021-02-28 16:47:14 +0100

    retouche slider

--- Front/src/components/Slider/index.js
+++ Front/src/components/Slider/index.js
@@ -4,40 +4,35 @@ import { Link } from 'react-router-dom';
 import { Slide } from 'react-slideshow-image';
 import Slid from './slid';
 
-
 import './slide.scss';
 
 const proprietes = {
-    duration: 5000,
-    transitionDuration: 500,
-    infinite: true,
-    indicators: true,
-    pauseOnHover: true,
-    canSwipe: true,
-}
+  duration: 5000,
+  transitionDuration: 500,
+  infinite: true,
+  indicators: true,
+  pauseOnHover: true,
+  canSwipe: true,
+};
 
 const Slider = ({ manageLoad, slider }) => {
   useEffect(
     manageLoad,
     [],
   );
-  
 
   return (
-        
-        
-            <Slide {...proprietes}>
-            {
+
+    <Slide {...proprietes}>
+      {
             slider.map((slid) => (
               <Slid key={slid.id} {...slid} />
 
             ))
           }
-            </Slide>
-            
-       
-    )
-}
+    </Slide>
+
+  );
+};
 
 export default Slider;
-
--- Front/src/components/Slider/slid.js
+++ Front/src/components/Slider/slid.js
@@ -4,7 +4,6 @@ import imgUrl from 'src/utils/imgUrl';
 
 import './slide.scss';
 
-
 const Slid = ({ id, picture }) => (
 
   <img id={id} className="slider__img" src={`${imgUrl()}${picture}`} alt="" />
