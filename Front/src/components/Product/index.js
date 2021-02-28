e5e3dfb8d8628a04958edf33ecb5d13292a0a7fa
Nicolas-PAIRON <niquaula@gmail.com>
2021-02-28 15:24:34 +0100

    modify product page

--- Front/src/components/Product/index.js
+++ Front/src/components/Product/index.js
@@ -5,20 +5,31 @@ import PropTypes from 'prop-types';
 import './Product.scss';
 import { addToCard } from '../../actions/shop';
 
+const Product = ({ product, isLogged }) => {
+  console.log(product); 
+  const dispatch = useDispatch();
+  const handleAddToCart = (product) => {
+    dispatch(addToCard(product));
+  };
+
+
+
 function handleClick__un() {
   const general = document.getElementById('general');
-  general.className = 'product__box__img--un image__un';
+  general.setAttribute("src", "http://0.0.0.0:8000/pictures/" + product.picture1);
 }
 
 function handleClick__deux() {
   const general = document.getElementById('general');
-  general.className = 'product__box__img--un image__deux';
+  general.setAttribute("src", "http://0.0.0.0:8000/pictures/" + product.picture2);
 }
+
 
 function handleClick__trois() {
   const general = document.getElementById('general');
-  general.className = 'product__box__img--un image__trois';
+  general.setAttribute("src", "http://0.0.0.0:8000/pictures/" + product.picture3);
 }
+
 function handleClick__fav() {
   const btnFav = document.getElementById('fav');
   let txtFav = document.querySelector('product__box__cart--btn-fav-txt');
@@ -32,38 +43,33 @@ function handleClick__fav() {
   }
 }
 
-const Product = ({ product, isLogged }) => {
-  console.log(product);
-  const dispatch = useDispatch();
-  const handleAddToCart = (product) => {
-    dispatch(addToCard(product));
-  };
-  return (
 
-    <div className="product">
-      <div className="product__box__img ">
-        <div className="product__box__img--un image__un" id="general" />
-        <div className="product__box__img__sub">
-          <div className="product__box__img__sub--unBis image__un " id="bis" onClick={handleClick__un} onMouseOver={handleClick__un} />
-          <div className="product__box__img__sub--deux image__deux" id="deux" onClick={handleClick__deux} onMouseOver={handleClick__deux} />
-          <div className="product__box__img__sub--trois image__trois" id="trois" onClick={handleClick__trois} onMouseOver={handleClick__trois} />
-        </div>
+return (
+  <div className="product">
+    <div className="product__box__img ">
+      <img src={`http://0.0.0.0:8000/pictures/` + product.picture1} className="product__box__img--un image__un" id="general" />
+      <div className="product__box__img__sub">
+        <img src={`http://0.0.0.0:8000/pictures/` + product.picture1} className="product__box__img__sub--unBis image__un " id="bis" onClick={handleClick__un} onMouseOver={handleClick__un} />
+        {product.picture2 != null &&(<img src={`http://0.0.0.0:8000/pictures/` + product.picture2} className="product__box__img__sub--deux image__deux" id="deux" onClick={handleClick__deux} onMouseOver={handleClick__deux} />)}
+        {product.picture3 != null &&(<img src={`http://0.0.0.0:8000/pictures/` + product.picture3} className="product__box__img__sub--trois image__trois" id="trois" onClick={handleClick__trois} onMouseOver={handleClick__trois} />)}
       </div>
-      <div className="product__box__txt">
-        <div className="product__box__txt--title"><h5>{product.name}</h5></div>
-        <div className="product__box__cart--price" id="prix">{product.price} Euros</div>
-        <div className="product__box__txt--quantite">Il reste {product.stock} exemplaires en stock</div>
+    </div>
+    <div className="product__box__txt">
+      <div className="product__box__txt--title"><h5>{product.name}</h5></div>
+      <div className="product__box__cart--price" id="prix">{product.price !=null &&(product.price.toLocaleString('fr-FR', { style: 'currency', currency: 'EUR' }))}</div>
+      <div className="product__box__txt--quantite">Il reste {product.stock} exemplaires en stock</div>
 
-        <div className="product__box__cart">
-        <input className="product__box__cart--number" type="number" id="number" name="number" min="0" max="100" placeholder="Quantité" />
-        <button type="button" onClick={() => handleAddToCart(product)} className="product__box__cart--btn-add">Ajouter au panier</button>
-        <button type="submit" className="product__box__cart--btn-fav" id="fav" onClick={handleClick__fav} ><div className="product__box__cart--btn-fav-txt">Ajouter aux favoris</div></button>
+      <div className="product__box__cart">
+      <input className="product__box__cart--number" type="number" id="number" name="number" min="0" max="100" placeholder="Quantité" />
+      <button type="button" onClick={() => handleAddToCart(product)} className="product__box__cart--btn-add">Ajouter au panier</button>
+      <button type="submit" className="product__box__cart--btn-fav" id="fav" onClick={handleClick__fav} ><div className="product__box__cart--btn-fav-txt">Ajouter aux favoris</div></button>
+    </div>
+      <div className="product__box__txt--description">{product.description}</div>
       </div>
-        <div className="product__box__txt--description">{product.description}</div>
-        </div>
-        </div>
-  );
+      </div>
+);
 };
+
 Product.propTypes = {
   product: PropTypes.object.isRequired,
 };
