54bdb01f5f9549f2b2e0411f475e0084db3b3f74
PiMa31 <pierre.masset@bbox.fr>
2021-03-02 13:27:26 +0100

    validate contact form

--- Front/src/components/Contact/index.js
+++ Front/src/components/Contact/index.js
@@ -11,11 +11,68 @@ const Contact = ({
   message,
   changeField,
   handleContact,
+  emptyInput,
+  badMail,
 }) => {
   const handleFormContactSubmit = (evt) => {
     evt.preventDefault();
     handleContact();
   };
+  // validation lastname input
+  function lastnameIsEmpty() {
+    const str = lastname;
+    if (!str.replace(/\s+/, '').length || !str) {
+      return false;
+    }
+    return true;
+  }
+  function lastnameTooLong() {
+    const str = lastname;
+    if (str.length > 50) {
+      return false;
+    }
+    return true;
+  }
+  // validation firstname input
+  function firstnameIsEmpty() {
+    const str = firstname;
+    if (!str.replace(/\s+/, '').length || !str) {
+      return false;
+    }
+    return true;
+  }
+  function firstnameTooLong() {
+    const str = firstname;
+    if (str.length > 50) {
+      return false;
+    }
+    return true;
+  }
+  // validation email input
+  function emailIsEmpty() {
+    const str = email;
+    if (!str.replace(/\s+/, '').length || !str) {
+      return false;
+    }
+    return true;
+  }
+  // validation message
+  function messageIsEmpty() {
+    const str = message;
+    if (!str.replace(/\s+/, '').length || !str) {
+      return false;
+    }
+    return true;
+  }
+  function messageTooLong() {
+    const str = message;
+    if (str.length > 8000) {
+      return false;
+    }
+    return true;
+  }
+  const regexEmail = new RegExp("^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$");
+  const msgTooLong = '--- Maximum de caractères atteint ---';
   return (
     <>
       <div className="title__contact"><h3>Formulaire de contact</h3></div>
@@ -28,6 +85,8 @@ const Contact = ({
           onChange={changeField}
           value={lastname}
         />
+        {!lastnameIsEmpty() && <div className="errorDiv">{emptyInput}</div>}
+        {!lastnameTooLong() && <div className="errorDiv">{msgTooLong}</div>}
 
         <Field
           name="firstname"
@@ -35,19 +94,24 @@ const Contact = ({
           onChange={changeField}
           value={firstname}
         />
+        {!firstnameIsEmpty() && <div className="errorDiv">{emptyInput}</div>}
+        {!firstnameTooLong() && <div className="errorDiv">{msgTooLong}</div>}
         <Field
           name="email"
           placeholder="Votre email"
           onChange={changeField}
           value={email}
         />
-
+        {!emailIsEmpty() && <div className="errorDiv">{emptyInput}</div>}
+        {!regexEmail.test(email) && <div className="errorDiv">{badMail}</div>}
         <Textarea
           name="message"
           placeholder="Votre message"
           onChange={changeField}
           value={message}
         />
+        {!messageIsEmpty() && <div className="errorDiv">{emptyInput}</div>}
+        {!messageTooLong() && <div className="errorDiv">{msgTooLong}</div>}
         <button type="submit" className="contact__form--button"><span>Envoyer</span></button>
       </form>
     </>
