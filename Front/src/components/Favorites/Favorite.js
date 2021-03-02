e20d6f06fb61f6950e2f5c29cb83496bb71e3b7e
BenoitLetessier <ciib@free.fr>
2021-03-02 11:47:37 +0100

    Front BL update favorites

--- Front/src/components/Favorites/Favorite.js
+++ Front/src/components/Favorites/Favorite.js
@@ -2,9 +2,10 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import { useDispatch } from 'react-redux';
 import { addToFavorite, removeFavorite } from '../../actions/favoritesAdd';
+import imgUrl from 'src/utils/imgUrl';
 
-const Favorite = ({ name, id }) => {
-  console.log(id);
+const Favorite = ({ name, id, picture1 }) => {
+  console.log(picture1);
   const dispatch = useDispatch();
   const handleRemoveFavorite = (id) => {
     dispatch(addToFavorite(id));
@@ -14,7 +15,8 @@ const Favorite = ({ name, id }) => {
 
   return (
     <div className="favorites__list--item">
-      <a className="favorites__list--item--name" href={pathUrl}><h5>{name}</h5></a>
+       <img className="col-card" src={`${imgUrl()}${picture1}`} alt="" />
+      <a className="favorites__list--item--name" href={pathUrl}>{name}</a>
       <button type="button" onClick={() => handleRemoveFavorite(id)} className="favorites__list--item--button"><span>Supprimer</span></button>
     </div>
   );
