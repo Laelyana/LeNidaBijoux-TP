fd16f75446fb51744319822782a420b1829caec2
BenoitLetessier <ciib@free.fr>
2021-03-02 10:48:24 +0100

    Front BL update dashboard

--- Front/src/components/Dashboard/index.js
+++ Front/src/components/Dashboard/index.js
@@ -3,21 +3,36 @@ import React from 'react';
 import './dashboard.scss';
 
 const Dashboard = () => (
+
   <div className="dashboard">
-    <h2> Mon tableau de bord</h2>
-    <div className="dashboard__items">
-      <div className="dashboard__items--haut profil"></div>
-      <div className="dashboard__items--bas"><a href="mon-compte/mes-infos"><h4>Mon profil</h4></a></div>
-      </div>
-      <div className="dashboard__items">
-      <div className="dashboard__items--haut favoris"></div>
-      <div className="dashboard__items--bas"><a href="mon-compte/mes-favoris"><h4>Mes favoris</h4></a></div>
-      </div>
-      <div className="dashboard__items">
-      <div className="dashboard__items--haut commandes"></div>
-      <div className="dashboard__items--bas"><a href="mon-compte/mes-commandes"><h4>Mes commandes</h4></a></div>
-      </div>
+
+    <h2 className="dashboard--title"> Mon tableau de bord</h2>
+
+    <div className="cat cat__img cat__img--un">
+  <div className="cat__center">
+    <div className="prod__band">
+      <div className="cat-card--title-band"><h4><a href="mon-compte/mes-infos">Mes infos</a></h4></div>
     </div>
+  </div>
+</div>
+
+<div className="cat cat__img cat__img--deux">
+  <div className="cat__center">
+    <div className="prod__band">
+    <div className="cat-card--title-band"><h4><a href="mon-compte/mes-favoris">Mes favoris</a></h4></div>
+    </div>
+  </div>
+</div>
+
+<div className="cat cat__img cat__img--trois">
+  <div className="cat__center">
+    <div className="prod__band">
+    <div className="cat-card--title-band"><h4><a href="mon-compte/mes-commandes">Mes commandes</a></h4></div>
+    </div>
+  </div>
+</div>
+
+</div>
 );
 
 
