1148dc185c9c169a89770ab80ef3a6debdb30c9d
PiMa31 <pierre.masset@bbox.fr>
2021-03-03 10:16:58 +0100

    empty error msg

--- Front/src/reducers/createUser.js
+++ Front/src/reducers/createUser.js
@@ -1,5 +1,5 @@
 import { CHANGE_NEW_USER_FIELD, SAVE_NEW_USER_DATA, CREATE_USER } from 'src/actions/user';
-import { ERR_MSG_CREATE_USER } from '../actions/user';
+import { ERR_MSG_CREATE_USER, LOG_IN } from '../actions/user';
 
 const initialState = {
   email: '',
@@ -51,6 +51,20 @@ export default (state = initialState, action = {}) => {
         badMail: '--- Email invalide ---',
         badPhone: '--- Ce champ ne doit contenir que des chiffres ---',
         badPassword: '--- Les mots de passe ne correspondent pas ---',
+      };
+    case LOG_IN:
+      return {
+        ...state,
+        email: '',
+        password: '',
+        firstname: '',
+        lastname: '',
+        phoneNumber: '',
+        emptyInput: '',
+        badMail: '',
+        badPhone: '',
+        badPassword: '',
+        confirmPassword: '',
       };
     default:
       return {
