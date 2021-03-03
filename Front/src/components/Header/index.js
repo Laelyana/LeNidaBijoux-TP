82e8cac0f33b57c187b51d0dbcdfe504865fc110
PiMa31 <pierre.masset@bbox.fr>
2021-03-03 17:03:51 +0100

    update

--- Front/src/components/Header/index.js
+++ Front/src/components/Header/index.js
@@ -1,19 +1,19 @@
 import React from 'react';
+import { NavLink } from 'react-router-dom';
 import logo from './logo.png';
 
 import './header.scss';
-import { NavLink } from 'react-router-dom';
 
 const Header = () => (
-    <div className="navigation">
-      <ul className="navigation__ul">
-        <NavLink to="/" activeClassName="selected" exact><li className="navigation__li">Accueil</li></NavLink>
-        <NavLink to="/l-atelier" activeClassName="selected" exact><li className="navigation__li">L'atelier</li></NavLink>
-        <NavLink to="/"><img src={logo} alt="logo" className="navigation__logo" /></NavLink>
-        <NavLink to="/Categories" activeClassName="selected" exact><li className="navigation__li">Catégories</li></NavLink>
-        <NavLink to="/Collections" activeClassName="selected" exact><li className="navigation__li">Collections</li></NavLink>
-      </ul>
-    </div>
+  <div className="navigation">
+    <ul className="navigation__ul">
+      <NavLink to="/" activeClassName="selected" exact><li className="navigation__li">Accueil</li></NavLink>
+      <NavLink to="/l-atelier" activeClassName="selected" exact><li className="navigation__li">L'atelier</li></NavLink>
+      <NavLink to="/"><img src={logo} alt="logo" className="navigation__logo" /></NavLink>
+      <NavLink to="/Categories" activeClassName="selected" exact><li className="navigation__li">Catégories</li></NavLink>
+      <NavLink to="/Collections" activeClassName="selected" exact><li className="navigation__li">Collections</li></NavLink>
+    </ul>
+  </div>
 );
 
 export default Header;
