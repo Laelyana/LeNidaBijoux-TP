30686b00dc33f918d06f2a1031115fd6f8254018
PiMa31 <pierre.masset@bbox.fr>
2021-03-03 16:53:29 +0100

    header new css

--- Front/src/components/Header/index.js
+++ Front/src/components/Header/index.js
@@ -2,19 +2,18 @@ import React from 'react';
 import logo from './logo.png';
 
 import './header.scss';
+import { NavLink } from 'react-router-dom';
 
 const Header = () => (
-  
     <div className="navigation">
       <ul className="navigation__ul">
-        <a href="/"><li className="navigation__li">Accueil</li></a>
-        <a href="/l-atelier"><li className="navigation__li">L'atelier</li></a>
-        <a href="/"><img  src={logo} alt="logo" className="navigation__logo"/></a>  
-        <a href="/Categories"><li className="navigation__li">Catégories</li></a>
-        <a href="/Collections"><li className="navigation__li">Collections</li></a>
+        <NavLink to="/" activeClassName="selected" exact><li className="navigation__li">Accueil</li></NavLink>
+        <NavLink to="/l-atelier" activeClassName="selected" exact><li className="navigation__li">L'atelier</li></NavLink>
+        <NavLink to="/"><img src={logo} alt="logo" className="navigation__logo" /></NavLink>
+        <NavLink to="/Categories" activeClassName="selected" exact><li className="navigation__li">Catégories</li></NavLink>
+        <NavLink to="/Collections" activeClassName="selected" exact><li className="navigation__li">Collections</li></NavLink>
       </ul>
     </div>
-
 );
 
 export default Header;
